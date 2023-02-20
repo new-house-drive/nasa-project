a20b89c97d4af8a6c200717f3ed7bf6bdfd69e55
Valerchik <new-house-drive@protonmail.com>
2023-02-20 02:59:15 +0200

    add async function to load kepler data

--- server/src/server.js
+++ server/src/server.js
@@ -5,12 +5,20 @@ const app = require('./app.js')
 const PORT = process.env.PORT || 8000
 const server = http.createServer(app)
 
-server.listen(PORT, () => {
-    try{
-    //throw new Error('Vasha mamka')
-    console.log(`✔️  It works, for now. ${PORT}`)
-    }
-    catch(error) {
-        console.log(`💀  ${error} is threating your app!`)
-    }
-})
+const {loadPlanetsData} = require('./models/planets.model')
+
+async function startServer() {
+    await loadPlanetsData();
+    server.listen(PORT, () => {
+        try{
+        //throw new Error('Vasha mamka')
+        console.log(`✔️  It works, for now. ${PORT}`)
+        }
+        catch(error) {
+            console.log(`💀  ${error} is threating your app!`)
+        }
+    })
+
+}
+
+startServer()
