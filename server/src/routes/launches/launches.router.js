1ea07a4d6336ae1c2b8a61a249cc86dd6c421f3c
Valerchik <new-house-drive@protonmail.com>
2023-02-27 03:54:35 +0200

    add router.delete

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,9 +1,10 @@
 const express = require('express')
 
 const router = express.Router()
-const { httpGetAllLaunches, httpAddNewLaunch } = require('./launches.controller')
+const { httpGetAllLaunches, httpAddNewLaunch, httpAbortLaunch } = require('./launches.controller')
 
 router.get('/', httpGetAllLaunches)
 router.post('/', httpAddNewLaunch)
+router.delete('/:id', httpAbortLaunch)
 
 module.exports = router
