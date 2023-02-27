d30072de5667a9199d1c3743bb5342abb5f81e24
Valerchik <new-house-drive@protonmail.com>
2023-02-27 02:20:22 +0200

    use API URL in links

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,20 +1,32 @@
 const API_URL = 'http://localhost:8000/'
 
 async function httpGetPlanets() {
-  const response = await fetch(`http://localhost:8000/planets`)
+  const response = await fetch(`${API_URL}planets`)
   return await response.json()
   // Load planets and return as JSON.
 }
 
 async function httpGetLaunches() {
-  const response = await fetch('http://localhost:8000/launches')
+  const response = await fetch(`${API_URL}launches`)
   const fethedLaunches = await response.json()
   return fethedLaunches.sort((a, b) => a.flightNumber - b.flightNumber) 
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(launch)
+    })
+  }
+  catch (error) {
+    return {
+      ok: false
+    }
+  }
 }
 
 async function httpAbortLaunch(id) {
