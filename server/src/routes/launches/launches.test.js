33aff2e22e2fb0a4d2b9c33d9db558efdf870ae0
Valerchik <new-house-drive@protonmail.com>
2023-02-27 21:13:57 +0200

    add test launches

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,20 @@
+describe('Test GET launches', () => {
+    test('Expect to be 200', ()=>{
+        const response = 200
+        expect(response).toBe(200)
+    })
+})
+
+describe('Test adding a launch to collection', () => {
+    test('Should return 201 Created', ()=>{
+
+    })
+
+    test('Should return error the parameter is missing.', ()=>{
+
+    })
+
+    test('Should return error the date is invalid.', ()=>{
+
+    })
+})
