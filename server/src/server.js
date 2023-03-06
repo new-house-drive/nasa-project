8abc154c83e180325f8ca65857d9dd5c00f7e2c4
Valerchik <new-house-drive@protonmail.com>
2023-03-06 23:12:26 +0200

    add mongo connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,25 @@
 const http = require('http')
 const app = require('./app.js')
+const mongoose = require('mongoose')
 
+const MONGO_URL = "mongodb+srv://superuser:tqqVfjRN7fGBH7sO@tocl-xvi.poxwtez.mongodb.net/?retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 8000
 const server = http.createServer(app)
 
 const {loadPlanetsData} = require('./models/planets.model')
 
+mongoose.connection.once('open', () => {
+    console.log('🧠 MongoDB connection ready!')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error(err)
+})
+
 async function startServer() {
+
+    mongoose.connect(MONGO_URL)
     await loadPlanetsData();
     server.listen(PORT, () => {
         try{
