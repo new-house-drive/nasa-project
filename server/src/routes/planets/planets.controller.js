b60a865b4d5e52804dcc8faf0b117dd8bfd3ec37
Valerchik <new-house-drive@protonmail.com>
2023-03-11 13:19:29 +0200

    getAllPlanets -> httpGetAllPlanents()

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,9 +1,9 @@
-const {planets} = require('../../models/planets.model')
+const {getAllPlanets} = require('../../models/planets.model')
 
-function getAllPlanets(req, res) {
-    res.status(200).json(planets)
+async function httpGetAllPlanets(req, res) {
+    res.status(200).json(await getAllPlanets())
 }
 
 module.exports = {
-    getAllPlanets
+    httpGetAllPlanets
 }
