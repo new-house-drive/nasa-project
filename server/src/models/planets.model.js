dce4b9d235e8317609d25b73252acd06d828df59
Valerchik <new-house-drive@protonmail.com>
2023-03-11 13:19:04 +0200

    add mongo updateOne

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,14 +1,24 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 const path = require("path");
-const planets = require('./planets.mongo')
+const planets = require("./planets.mongo");
 
-const habitablePlanets = []
+const habitablePlanets = [];
 
-const KEPLER_DATA_URL = path.join(__dirname, '..', '..', 'data', 'kepler_data.csv' )
+const KEPLER_DATA_URL = path.join(
+  __dirname,
+  "..",
+  "..",
+  "data",
+  "kepler_data.csv"
+);
 // Service Functions
 function isHabitablePlanet(planet) {
-  if (isEnoughInsolationFlux(planet) && isDispositionCOnfirmed(planet) && isRadiusOK(planet)) {
+  if (
+    isEnoughInsolationFlux(planet) &&
+    isDispositionCOnfirmed(planet) &&
+    isRadiusOK(planet)
+  ) {
     return planet;
   }
 }
@@ -25,45 +35,47 @@ function isEnoughInsolationFlux(planet) {
 }
 
 function isRadiusOK(planet) {
-    if (planet['koi_prad'] > 1.6) return
-    return planet  
+  if (planet["koi_prad"] > 1.6) return;
+  return planet;
 }
 
 // Receiving data
-function loadPlanetsData(){
+function loadPlanetsData() {
   return new Promise((resolve, reject) => {
     fs.createReadStream(KEPLER_DATA_URL)
-  // readable.pipe(writable)
-  .pipe(
-    parse({
-      comment: "#",
-      columns: true,
-    })
-  )
-  .on("data", async (planet) => {
-    if (isHabitablePlanet(planet)) {
-      habitablePlanets.push(planet);
-      // TODO: replace the code with upsert
-      // insert + update = upsert
-      // await planets.create({
-      //   keplerName: planet.kepler_name
-      // })
-    }
-  })
-  .on("end", () => {
-    console.log("☪️ We are done!");
-    resolve();
-  })
-  .on("error", (err) => {
-    console.log("Error!");
-    console.log(err);
-    reject(err)
+      // readable.pipe(writable)
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", async (planet) => {
+        if (isHabitablePlanet(planet)) {
+          habitablePlanets.push(planet);
+          // TODO: replace the code with upsert
+          // insert + update = upsert
+          await planets.updateOne({
+            keplerName: planet.kepler_name
+          })
+        }
+      })
+      .on("end", () => {
+        console.log("☪️ We are done!");
+        resolve();
+      })
+      .on("error", (err) => {
+        console.log("Error!");
+        console.log(err);
+        reject(err);
+      });
   });
+}
 
-  })
-
+async function getAllPlanets() {
+  return await planets.find();
 }
 module.exports = {
-    planets: habitablePlanets,
-    loadPlanetsData: loadPlanetsData,
-}
+  planets: getAllPlanets,
+  loadPlanetsData: loadPlanetsData,
+};
