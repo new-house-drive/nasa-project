d5d3125fb5b4caf33ccf36dd006e7a1082aa153b
Valerchik <new-house-drive@protonmail.com>
2023-03-21 00:58:34 +0200

    make httpAddNewLaunch an async function

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -5,7 +5,7 @@ async function httpGetAllLaunches(req, res) {
     return res.status(200).json(await getAllLaunches())
 }
 
-function httpAddNewLaunch(req, res) {
+async function httpAddNewLaunch(req, res) {
     const launch = req.body
     if (!launch.mission 
         || !launch.target 
@@ -26,7 +26,7 @@ function httpAddNewLaunch(req, res) {
     }
 
 
-    scheduleLaunch(launch)
+    await scheduleLaunch(launch)
     return res.status(201).json(launch)
 
 }
