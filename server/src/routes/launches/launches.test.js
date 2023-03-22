436d2ad1eed8015e18ec4a5eced717581c2c193e
Valerchik <new-house-drive@protonmail.com>
2023-03-23 01:03:58 +0200

    add beforeAll tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,75 +1,83 @@
 const request = require("supertest");
 const app = require("../../app");
+const { mongoConnect } = require("../../services/mongo");
 
-describe("Test GET launches", () => {
-  test("Expect to be 200", async () => {
-    await request(app)
-      .get("/launches")
-      .expect("Content-Type", /json/)
-      .expect(200);
-    // expect(response.statusCode).toBe(200)
-  });
-});
-
-describe("Test adding a launch to collection", () => {
-  completeLaunchData = {
-    mission: "Olena Metelyk Vision",
-    target: "Dmytro Faliush Enterprise",
-    rocket: "Hex",
-    launchDate: "January 17, 2042",
-  };
-
-  launchDataWithoutDate = {
-    mission: "Olena Metelyk Vision",
-    target: "Dmytro Faliush Enterprise",
-    rocket: "Hex",
-  };
-
-  launchDataWithoutTarget = {
-    mission: "Olena Metelyk Vision",
-    rocket: "Hex",
-    launchDate: "January 17, 2042",
-  };
-
-  launchDataWithInvalidDate = {
-    mission: "Olena Metelyk Vision",
-    target: "Dmytro Faliush Enterprise",
-    rocket: "Hex",
-    launchDate: "Будут бальшиє? Будут і бальшиє в том чіслє.",
-  };
-
-  test("Should return 201 Created", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(completeLaunchData)
-      .expect("Content-Type", /json/)
-      .expect(201);
-    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
-    const responseDate = new Date(response.body.launchDate).valueOf();
-    expect(responseDate).toBe(requestDate);
-
-    expect(response.body).toMatchObject(launchDataWithoutDate);
-  });
-
-  test("Should return error the parameter is missing.", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(launchDataWithoutTarget)
-      .expect(400);
-
-    expect(response.body).toStrictEqual({
-      error: "Missing information!  🥺👉👈",
+describe("Test Launches API", () => {
+  beforeAll(async ()=> {
+    await mongoConnect();
+  })
+  
+  describe("Test GET launches", () => {
+    test("Expect to be 200", async () => {
+      mongoConnect();
+      await request(app)
+        .get("/launches")
+        .expect("Content-Type", /json/)
+        .expect(200);
+      // expect(response.statusCode).toBe(200)
     });
   });
 
-  test("Should return error the date is invalid.", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(launchDataWithInvalidDate)
-      .expect(400);
+  describe("Test adding a launch to collection", () => {
+    completeLaunchData = {
+      mission: "Olena Metelyk Vision",
+      target: "Dmytro Faliush Enterprise",
+      rocket: "Hex",
+      launchDate: "January 17, 2042",
+    };
 
-    expect(response.body).toStrictEqual({
-      error: "Date is invalid!  🥺👉👈",
+    launchDataWithoutDate = {
+      mission: "Olena Metelyk Vision",
+      target: "Dmytro Faliush Enterprise",
+      rocket: "Hex",
+    };
+
+    launchDataWithoutTarget = {
+      mission: "Olena Metelyk Vision",
+      rocket: "Hex",
+      launchDate: "January 17, 2042",
+    };
+
+    launchDataWithInvalidDate = {
+      mission: "Olena Metelyk Vision",
+      target: "Dmytro Faliush Enterprise",
+      rocket: "Hex",
+      launchDate: "Будут бальшиє? Будут і бальшиє в том чіслє.",
+    };
+
+    test("Should return 201 Created", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(completeLaunchData)
+        .expect("Content-Type", /json/)
+        .expect(201);
+      const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+      const responseDate = new Date(response.body.launchDate).valueOf();
+      expect(responseDate).toBe(requestDate);
+
+      expect(response.body).toMatchObject(launchDataWithoutDate);
+    });
+
+    test("Should return error the parameter is missing.", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(launchDataWithoutTarget)
+        .expect(400);
+
+      expect(response.body).toStrictEqual({
+        error: "Missing information!  🥺👉👈",
+      });
+    });
+
+    test("Should return error the date is invalid.", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(launchDataWithInvalidDate)
+        .expect(400);
+
+      expect(response.body).toStrictEqual({
+        error: "Date is invalid!  🥺👉👈",
+      });
     });
   });
 });
