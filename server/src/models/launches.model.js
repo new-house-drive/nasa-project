c11f14f0ce1b5985dd0a355edfec2d97d7acd639
Valerchik <new-house-drive@protonmail.com>
2023-04-01 15:56:55 +0300

    add receiving the data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -150,12 +150,31 @@ async function loadLaunchesData() {
         {
           path: "payloads",
           select: {
-            customers: 1
-          }
-        }
+            customers: 1,
+          },
+        },
       ],
     },
   });
+
+  const launchDocs = response.data.docs;
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc["payloads"];
+    const customers = payloads.flatMap((payload) => {
+      return payload["customers"];
+    });
+
+    const launch = {
+      flightNumber: launchDoc["flight_number"],
+      mission: launchDoc["name"],
+      rocket: launchDoc["rocket"]["name"],
+      launchDate: launchDoc["date_local"],
+      upcoming: launchDoc["upcoming"],
+      success: launchDoc["success"],
+      customers: customers,
+    };
+    console.log(launch.flightNumber, launch.mission)
+  }
 }
 
 // function addLaunch(newLaunch) {
