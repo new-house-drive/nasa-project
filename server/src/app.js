3cf9614f5a2b1c810fec9c959fee08de5bd55fff
Valerchik <new-house-drive@protonmail.com>
2023-05-07 19:41:30 +0300

    build

--- server/src/app.js
+++ server/src/app.js
@@ -12,6 +12,7 @@ app.use(cors({
 app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')))
 
 app.use('/v1', api)
 app.get('/*', (req, res) => {
